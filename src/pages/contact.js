a3616a5901bf95c5e70c8e334b165cd52df5c7ce
balaitbalagithubrit <c.l.bala20@gmail.com>
2024-07-25 03:18:37 +0530

    initial commit3

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,14 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Contact = () => (
   <div style={{height:'100vh'}}>
     <Navbar />
     <h1>Contact Page</h1>
+      <Image
+      src='/freshness-beauty-nature-wet-drops-generated-by-ai.jpg'
+      alt='Nature'
+    />
   </div>
 );
 
